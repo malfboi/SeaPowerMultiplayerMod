9684619058bc3749761a8c1553ecf950a441f59b
Barnaby Hughes <hughesbarney@gmail.com>
2026-08-08 21:00:08 +0100

    bump protocol

--- src/Net2/ProtocolInfo.cs
+++ src/Net2/ProtocolInfo.cs
@@ -9,7 +9,7 @@ namespace SeapowerMultiplayer.Net2
     /// </summary>
     public static class ProtocolInfo
     {
-        public const ushort ProtocolVersion = 228; // JamState + JamSystem order (offensive ECM)
+        public const ushort ProtocolVersion = 229; // Various fixes
 
         /// <summary>
         /// The Sea Power build both players are running. Save files embed indices
